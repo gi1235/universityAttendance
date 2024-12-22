d0b00ff62ec2738fc84f7a14ab576810e7ccafbe
gi1235 <146915283+gi1235@users.noreply.github.com>
2024-12-22 11:11:58 +0900

    1차원배열에서 2차원 배열로 변경

--- timeTable/src/timeTable/Main.java
+++ timeTable/src/timeTable/Main.java
@@ -6,38 +6,47 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String[] args){
         String myTimeTable = "0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0";
-        String[] timeTable = new String[45];
+
+        // 요일을 행 시간을 열로 배치
+        String[][] timeTable = new String[5][9];
+
         ArrayList<universiryTime> addTime = new ArrayList<>();
         addTime.add(new universiryTime("math", 3, 3));
         addTime.add(new universiryTime("korean", 8, 2));
         addTime.add(new universiryTime("english", 3, 2));
         addTime.add(new universiryTime("math", 4, 3));
         StringTokenizer tk = new StringTokenizer(myTimeTable, ", ");
-        int i = 0;
-        while (tk.hasMoreTokens()) {
-            timeTable[i] = tk.nextToken();
-            i++;
+
+        for(int i=0 ; i <timeTable.length ; i++){
+            for(int j=0 ; j <timeTable[0].length; j++){
+                timeTable[i][j] = tk.nextToken();
+            }
         }
         
-        for(i = 0 ; i < addTime.size() ; i++){
-            if(timeTable[addTime.get(i).getTime()].equals("0") && ! checkTime(timeTable, addTime.get(i).getName())){
-                timeTable[addTime.get(i).getTime()] = addTime.get(i).getName();
+        for(int i = 0 ; i < addTime.size() ; i++){
+            if(timeTable[2][addTime.get(i).getTime()].equals("0") && !checkName(timeTable, addTime.get(i).getName()) ){
+                timeTable[2][addTime.get(i).getTime()] = addTime.get(i).getName();
             }
         }
-        for(i = 0; i <timeTable.length; i++){
-            if(i % 5 == 0){
-                System.out.println();
+
+        for(int i=0 ; i <timeTable.length ; i++){
+            for(int j=0 ; j <timeTable[0].length; j++){
+                System.out.print(timeTable[i][j]);
             }
-            System.out.print(timeTable[i]);
+            System.out.println();
         }
 
     }
-    public static boolean checkTime(String[] timeTable, String name){
-        for (String str : timeTable){
-            if(str.equals(name)){
-                return true;
+
+    public static boolean checkName(String[][] timeTable, String name){
+        for(int i =0 ; i < timeTable.length; i++){
+            for (String str : timeTable[i]){
+                if(str.equals(name)){
+                    return true;
+                }
             }
         }
+        
         return false;
     }
 }
