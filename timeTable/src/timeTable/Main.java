32b94a9a04d6baaec468df757e9eeaef40c8a9db
gi1235 <146915283+gi1235@users.noreply.github.com>
2024-12-23 22:47:51 +0900

    메인 클래스 주석 추가
    23일 마지막 커밋

--- timeTable/src/timeTable/Main.java
+++ timeTable/src/timeTable/Main.java
@@ -3,6 +3,28 @@ package timeTable;
 import java.util.ArrayList;
 import java.util.StringTokenizer;
 
+
+/**
+ * 프로그램의 기본 실행 클래스 입니다.
+ * 
+ * @author Lim Jonggu
+ * @version 1.5
+ * @since 1.0
+ * 
+ * @created 2024-12-22
+ * @lastModified 2024-12-23
+ * 
+ * @changelog
+ * 
+ * <ul>
+ *   <li>2024-12-22: 최초 생성 (Lim Jonggu)</li>
+ *   <li>2024-12-22: 기본 틀 제작 (Lim Jonggu)</li>
+ *   <li>2024-12-22: timeTable 배열 가독성 향상 (Lim Jonggu)</li>
+ *   <li>2024-12-23: timeTable 변경 알고리즘 추가 (Lim Jonggu)</li>
+ *   <li>2024-12-23: timeTable 예외 처리 부분 추가 (Lim Jonggu)</li>
+ * </ul>
+ */
+
 public class Main {
     static ArrayList<universityTime> addTime = new ArrayList<>();
     static ArrayList<String> dayoff = new ArrayList<>();
@@ -49,12 +71,6 @@ public class Main {
             for(int j = nowAdd.getTime() - 1 ; j < (nowAdd.getTime()-1) + nowAdd.getCredit() ; j++ ){
                 timeTable[week][j] = nowAdd.getName();
             }
-         }
-         for(int i=0 ; i <timeTable.length ; i++){
-            for(int j=0 ; j <timeTable[0].length; j++){
-                System.out.print(timeTable[i][j]+" ");
-            }
-            System.out.println();
         }
     }
 
